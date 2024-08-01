475ededba5fe4587a27e6da2ffe6f7a6da063859
Grant Barnes <gfbarnes78@gmail.com>
2024-08-01 18:57:09 -0500

    moving login button to base page

--- src/api/pages.go
+++ src/api/pages.go
@@ -10,30 +10,30 @@ import (
 )
 
 func PageHome(w http.ResponseWriter, req *http.Request) {
+	type PageDataHome struct {
+		LoggedIn   bool
+		PlayerName string
+	}
+
 	tmpl, err := template.ParseFiles("templates/pages/home.html", "templates/base.html")
 	if err != nil {
 		fmt.Fprintf(w, "failed to parse HTML\n")
 		return
 	}
 
-	type HomePageData struct {
-		LoggedIn   bool
-		PlayerName string
-	}
-
 	playerName, err := auth.GetPlayerName(req)
-	tmpl.ExecuteTemplate(w, "base", HomePageData{
+
+	tmpl.ExecuteTemplate(w, "base", PageDataHome{
 		LoggedIn:   err == nil,
 		PlayerName: playerName,
 	})
 }
 
 func PageLobbyJoin(w http.ResponseWriter, req *http.Request) {
-	dbcs := database.GetDatabaseConnectionString()
-	lobbies, err := database.GetLobbies(dbcs)
-	if err != nil {
-		fmt.Fprintf(w, "failed to connect to database\n")
-		return
+	type PageDataLobbyJoin struct {
+		LoggedIn   bool
+		PlayerName string
+		Lobbies    []database.Lobby
 	}
 
 	tmpl, err := template.ParseFiles("templates/pages/lobby/join.html", "templates/base.html")
@@ -42,15 +42,27 @@ func PageLobbyJoin(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	tmpl.ExecuteTemplate(w, "base", lobbies)
-}
-
-func PageCardList(w http.ResponseWriter, req *http.Request) {
 	dbcs := database.GetDatabaseConnectionString()
-	cards, err := database.GetJudgeCards(dbcs)
+	lobbies, err := database.GetLobbies(dbcs)
 	if err != nil {
 		fmt.Fprintf(w, "failed to connect to database\n")
 		return
+	}
+
+	playerName, err := auth.GetPlayerName(req)
+
+	tmpl.ExecuteTemplate(w, "base", PageDataLobbyJoin{
+		LoggedIn:   err == nil,
+		PlayerName: playerName,
+		Lobbies:    lobbies,
+	})
+}
+
+func PageCardList(w http.ResponseWriter, req *http.Request) {
+	type PageDataCardList struct {
+		LoggedIn   bool
+		PlayerName string
+		Cards      []database.Card
 	}
 
 	tmpl, err := template.ParseFiles("templates/cards.html", "templates/base.html")
@@ -59,5 +71,18 @@ func PageCardList(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	tmpl.ExecuteTemplate(w, "base", cards)
+	dbcs := database.GetDatabaseConnectionString()
+	cards, err := database.GetJudgeCards(dbcs)
+	if err != nil {
+		fmt.Fprintf(w, "failed to connect to database\n")
+		return
+	}
+
+	playerName, err := auth.GetPlayerName(req)
+
+	tmpl.ExecuteTemplate(w, "base", PageDataCardList{
+		LoggedIn:   err == nil,
+		PlayerName: playerName,
+		Cards:      cards,
+	})
 }
