b0e87816f5dc6d8bb3679bdf84261c9612ad31eb
Grant Barnes <gfbarnes78@gmail.com>
2024-08-02 13:49:48 -0500

    using playerName provided in login form

--- src/api/post.go
+++ src/api/post.go
@@ -8,13 +8,29 @@ import (
 )
 
 func PostLogin(w http.ResponseWriter, req *http.Request) {
-	playerName := "Grant"
+	req.ParseForm()
+	var playerName string
+	for key, val := range req.Form {
+		if key != "playerName" {
+			continue
+		}
+		playerName = val[0]
+		break
+	}
+
+	if playerName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no name found"))
+		return
+	}
+
 	err := auth.SetPlayerName(w, playerName)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("failure"))
+		w.Write([]byte("failed to set cookie"))
 		return
 	}
+
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(fmt.Sprintf("Welcome %s!", playerName)))
 }
