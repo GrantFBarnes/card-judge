e5286983cc8128668accea98ce15510eda27044f
Grant Barnes <gfbarnes78@gmail.com>
2024-09-08 18:02:31 -0500

    fixing htmx result coloring

--- src/static/js/global.js
+++ src/static/js/global.js
@@ -5,13 +5,14 @@ document.addEventListener("htmx:beforeOnLoad", function (event) {
 });
 
 document.addEventListener("htmx:afterSwap", function (event) {
-  if (event?.detail?.pathInfo?.requestPath?.indexOf("/search") >= 0) return;
-  if (event.detail.xhr.status >= 200 && event.detail.xhr.status < 300) {
-    addClassToTarget("htmx-result-good", event.detail.target);
-    removeClassFromTarget("htmx-result-bad", event.detail.target);
-  } else {
-    addClassToTarget("htmx-result-bad", event.detail.target);
-    removeClassFromTarget("htmx-result-good", event.detail.target);
+  if (event.detail.target.classList.contains("htmx-result")) {
+    if (event.detail.xhr.status >= 200 && event.detail.xhr.status < 300) {
+      addClassToTarget("htmx-result-good", event.detail.target);
+      removeClassFromTarget("htmx-result-bad", event.detail.target);
+    } else {
+      addClassToTarget("htmx-result-bad", event.detail.target);
+      removeClassFromTarget("htmx-result-good", event.detail.target);
+    }
   }
 });
 
