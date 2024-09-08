1a9910319afd4178ec2b67c386ec520561eb9ffd
Grant Barnes <gfbarnes78@gmail.com>
2024-09-08 15:12:40 -0500

    adding lobby chat

--- src/static/js/lobby.js
+++ src/static/js/lobby.js
@@ -8,15 +8,36 @@ window.onload = () => {
     wsProtocol + document.location.host + "/ws" + document.location.pathname
   );
 
+  if (!conn) {
+    alert("Failed to make connection.");
+    document.location.href = "/lobbies";
+  }
+
   conn.onclose = () => {
     alert("Connection Lost");
     document.location.href = "/lobbies";
   };
 
-  const chatbox = document.getElementById("chatbox");
+  const lobbyChatForm = document.getElementById("lobby-chat-form");
+  const lobbyChatMessages = document.getElementById("lobby-chat-messages");
+  const lobbyChatInput = document.getElementById("lobby-chat-input");
+
+  lobbyChatForm.onsubmit = () => {
+    if (!lobbyChatInput.value) return;
+    conn.send(lobbyChatInput.value);
+    lobbyChatInput.value = "";
+  };
+
   conn.onmessage = (event) => {
-    const message = document.createElement("p");
-    message.innerHTML = event.data;
-    chatbox.appendChild(message);
+    const message = document.createElement("div");
+    message.innerText = event.data;
+    lobbyChatMessages.appendChild(message);
+
+    while (lobbyChatMessages.childNodes.length > 100) {
+      lobbyChatMessages.removeChild(lobbyChatMessages.childNodes[0]);
+    }
+
+    lobbyChatMessages.scrollTop =
+      lobbyChatMessages.scrollHeight - lobbyChatMessages.clientHeight;
   };
 };
