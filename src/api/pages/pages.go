6cf92de9e8f81c5b8d64a5c9eebfe06dab6dcdd7
Grant Barnes <gfbarnes78@gmail.com>
2024-09-08 15:50:07 -0500

    reorganize api pages methods

--- src/api/pages/pages.go
+++ src/api/pages/pages.go
@@ -15,6 +15,9 @@ type homeData struct {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	basePageData := api.GetBasePageData(r)
+	basePageData.PageTitle = "Card Judge - Home"
+
 	tmpl, err := template.ParseFiles(
 		"templates/pages/base.html",
 		"templates/pages/body/home.html",
@@ -23,9 +26,6 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "failed to parse HTML\n")
 		return
 	}
-
-	basePageData := api.GetBasePageData(r)
-	basePageData.PageTitle = "Card Judge - Home"
 
 	tmpl.ExecuteTemplate(w, "base", homeData{
 		Data: basePageData,
@@ -37,19 +37,19 @@ type loginData struct {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	basePageData := api.GetBasePageData(r)
+	basePageData.PageTitle = "Card Judge - Login"
+
 	tmpl, err := template.ParseFiles(
+		"templates/components/forms/player-create-form.html",
+		"templates/components/forms/player-login-form.html",
 		"templates/pages/base.html",
 		"templates/pages/body/login.html",
-		"templates/components/forms/player-login-form.html",
-		"templates/components/forms/player-create-form.html",
 	)
 	if err != nil {
 		fmt.Fprintf(w, "failed to parse HTML\n")
 		return
 	}
-
-	basePageData := api.GetBasePageData(r)
-	basePageData.PageTitle = "Card Judge - Login"
 
 	tmpl.ExecuteTemplate(w, "base", loginData{
 		Data: basePageData,
@@ -61,21 +61,21 @@ type manageData struct {
 }
 
 func Manage(w http.ResponseWriter, r *http.Request) {
+	basePageData := api.GetBasePageData(r)
+	basePageData.PageTitle = "Card Judge - Manage"
+
 	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/manage.html",
-		"templates/components/forms/player-name-form.html",
-		"templates/components/forms/player-password-form.html",
 		"templates/components/forms/player-color-theme-form.html",
 		"templates/components/forms/player-logout-form.html",
+		"templates/components/forms/player-name-form.html",
+		"templates/components/forms/player-password-form.html",
+		"templates/pages/base.html",
+		"templates/pages/body/manage.html",
 	)
 	if err != nil {
 		fmt.Fprintf(w, "failed to parse HTML\n")
 		return
 	}
-
-	basePageData := api.GetBasePageData(r)
-	basePageData.PageTitle = "Card Judge - Manage"
 
 	tmpl.ExecuteTemplate(w, "base", manageData{
 		Data: basePageData,
@@ -88,17 +88,6 @@ type adminData struct {
 }
 
 func Admin(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/admin.html",
-		"templates/components/table-rows/player-table-rows.html",
-		"templates/components/forms/player-create-default-form.html",
-	)
-	if err != nil {
-		fmt.Fprintf(w, "failed to parse HTML\n")
-		return
-	}
-
 	players, err := database.GetPlayers()
 	if err != nil {
 		fmt.Fprintf(w, "failed to connect to database\n")
@@ -107,6 +96,17 @@ func Admin(w http.ResponseWriter, r *http.Request) {
 
 	basePageData := api.GetBasePageData(r)
 	basePageData.PageTitle = "Card Judge - Admin"
+
+	tmpl, err := template.ParseFiles(
+		"templates/components/forms/player-create-default-form.html",
+		"templates/components/table-rows/player-table-rows.html",
+		"templates/pages/base.html",
+		"templates/pages/body/admin.html",
+	)
+	if err != nil {
+		fmt.Fprintf(w, "failed to parse HTML\n")
+		return
+	}
 
 	tmpl.ExecuteTemplate(w, "base", adminData{
 		Data:    basePageData,
@@ -121,18 +121,6 @@ type lobbiesData struct {
 }
 
 func Lobbies(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/lobbies.html",
-		"templates/components/table-rows/lobby-table-rows.html",
-		"templates/components/dialogs/lobby-create-dialog.html",
-		"templates/components/forms/lobby-create-form.html",
-	)
-	if err != nil {
-		fmt.Fprintf(w, "failed to parse HTML\n")
-		return
-	}
-
 	lobbies, err := database.GetLobbies()
 	if err != nil {
 		fmt.Fprintf(w, "failed to connect to database\n")
@@ -145,6 +133,18 @@ func Lobbies(w http.ResponseWriter, r *http.Request) {
 	decks, err := database.GetPlayerDecks(basePageData.Player.Id)
 	if err != nil {
 		fmt.Fprintf(w, "failed to connect to database\n")
+		return
+	}
+
+	tmpl, err := template.ParseFiles(
+		"templates/components/dialogs/lobby-create-dialog.html",
+		"templates/components/forms/lobby-create-form.html",
+		"templates/components/table-rows/lobby-table-rows.html",
+		"templates/pages/base.html",
+		"templates/pages/body/lobbies.html",
+	)
+	if err != nil {
+		fmt.Fprintf(w, "failed to parse HTML\n")
 		return
 	}
 
@@ -162,19 +162,6 @@ type lobbyData struct {
 }
 
 func Lobby(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/lobby.html",
-		"templates/components/forms/lobby-access-form.html",
-		"templates/components/forms/lobby-name-form.html",
-		"templates/components/forms/lobby-password-form.html",
-		"templates/components/dialogs/lobby-update-dialog.html",
-	)
-	if err != nil {
-		fmt.Fprintf(w, "failed to parse HTML\n")
-		return
-	}
-
 	idString := r.PathValue("id")
 	id, err := uuid.Parse(idString)
 	if err != nil {
@@ -191,6 +178,19 @@ func Lobby(w http.ResponseWriter, r *http.Request) {
 	basePageData := api.GetBasePageData(r)
 	basePageData.PageTitle = "Card Judge - Lobby"
 
+	tmpl, err := template.ParseFiles(
+		"templates/components/dialogs/lobby-update-dialog.html",
+		"templates/components/forms/lobby-access-form.html",
+		"templates/components/forms/lobby-name-form.html",
+		"templates/components/forms/lobby-password-form.html",
+		"templates/pages/base.html",
+		"templates/pages/body/lobby.html",
+	)
+	if err != nil {
+		fmt.Fprintf(w, "failed to parse HTML\n")
+		return
+	}
+
 	tmpl.ExecuteTemplate(w, "base", lobbyData{
 		Data:      basePageData,
 		HasAccess: !lobby.PasswordHash.Valid || basePageData.Player.HasLobbyAccess(lobby.Id),
@@ -204,18 +204,6 @@ type decksData struct {
 }
 
 func Decks(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/decks.html",
-		"templates/components/table-rows/deck-table-rows.html",
-		"templates/components/dialogs/deck-create-dialog.html",
-		"templates/components/forms/deck-create-form.html",
-	)
-	if err != nil {
-		fmt.Fprintf(w, "failed to parse HTML\n")
-		return
-	}
-
 	decks, err := database.GetDecks()
 	if err != nil {
 		fmt.Fprintf(w, "failed to connect to database\n")
@@ -224,6 +212,18 @@ func Decks(w http.ResponseWriter, r *http.Request) {
 
 	basePageData := api.GetBasePageData(r)
 	basePageData.PageTitle = "Card Judge - Decks"
+
+	tmpl, err := template.ParseFiles(
+		"templates/components/dialogs/deck-create-dialog.html",
+		"templates/components/forms/deck-create-form.html",
+		"templates/components/table-rows/deck-table-rows.html",
+		"templates/pages/base.html",
+		"templates/pages/body/decks.html",
+	)
+	if err != nil {
+		fmt.Fprintf(w, "failed to parse HTML\n")
+		return
+	}
 
 	tmpl.ExecuteTemplate(w, "base", decksData{
 		Data:  basePageData,
@@ -239,25 +239,6 @@ type deckData struct {
 }
 
 func Deck(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles(
-		"templates/pages/base.html",
-		"templates/pages/body/deck.html",
-		"templates/components/forms/deck-access-form.html",
-		"templates/components/forms/deck-name-form.html",
-		"templates/components/forms/deck-password-form.html",
-		"templates/components/dialogs/deck-update-dialog.html",
-		"templates/components/table-rows/card-table-rows.html",
-		"templates/components/forms/card-create-form.html",
-		"templates/components/dialogs/card-create-dialog.html",
-		"templates/components/forms/card-type-form.html",
-		"templates/components/forms/card-text-form.html",
-		"templates/components/dialogs/card-update-dialog.html",
-	)
-	if err != nil {
-		fmt.Fprintf(w, "failed to parse HTML\n")
-		return
-	}
-
 	idString := r.PathValue("id")
 	id, err := uuid.Parse(idString)
 	if err != nil {
@@ -279,6 +260,25 @@ func Deck(w http.ResponseWriter, r *http.Request) {
 
 	basePageData := api.GetBasePageData(r)
 	basePageData.PageTitle = "Card Judge - Deck"
+
+	tmpl, err := template.ParseFiles(
+		"templates/components/dialogs/card-create-dialog.html",
+		"templates/components/dialogs/card-update-dialog.html",
+		"templates/components/dialogs/deck-update-dialog.html",
+		"templates/components/forms/card-create-form.html",
+		"templates/components/forms/card-text-form.html",
+		"templates/components/forms/card-type-form.html",
+		"templates/components/forms/deck-access-form.html",
+		"templates/components/forms/deck-name-form.html",
+		"templates/components/forms/deck-password-form.html",
+		"templates/components/table-rows/card-table-rows.html",
+		"templates/pages/base.html",
+		"templates/pages/body/deck.html",
+	)
+	if err != nil {
+		fmt.Fprintf(w, "failed to parse HTML\n")
+		return
+	}
 
 	tmpl.ExecuteTemplate(w, "base", deckData{
 		Data:      basePageData,
