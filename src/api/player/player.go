3cbb8aa246681c3fab1114668d4e1c7c3008a8e7
Grant Barnes <gfbarnes78@gmail.com>
2024-09-12 14:54:21 -0500

    auto set judge with database trigger to avoid manual setting

--- src/api/player/player.go
+++ src/api/player/player.go
@@ -64,34 +64,6 @@ func GetGameBoard(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "game-board", data)
 }
 
-func BecomeJudge(w http.ResponseWriter, r *http.Request) {
-	playerIdString := r.PathValue("playerId")
-	playerId, err := uuid.Parse(playerIdString)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Failed to get player id from path."))
-		return
-	}
-
-	playerData, err := database.PlayerBecomeJudge(playerId)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	tmpl, err := template.ParseFiles(
-		"templates/components/game/player-data.html",
-	)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to parse HTML."))
-		return
-	}
-
-	tmpl.ExecuteTemplate(w, "player-data", playerData)
-}
-
 func DrawPlayerHand(w http.ResponseWriter, r *http.Request) {
 	playerIdString := r.PathValue("playerId")
 	playerId, err := uuid.Parse(playerIdString)
--- src/main.go
+++ src/main.go
@@ -85,7 +85,6 @@ func main() {
 	// player
 	http.Handle("GET /api/player/{playerId}/player-data", api.ApiMiddleware(http.HandlerFunc(apiPlayer.GetPlayerData)))
 	http.Handle("GET /api/player/{playerId}/game-board", api.ApiMiddleware(http.HandlerFunc(apiPlayer.GetGameBoard)))
-	http.Handle("POST /api/player/{playerId}/become-judge", api.ApiMiddleware(http.HandlerFunc(apiPlayer.BecomeJudge)))
 	http.Handle("POST /api/player/{playerId}/draw", api.ApiMiddleware(http.HandlerFunc(apiPlayer.DrawPlayerHand)))
 	http.Handle("POST /api/player/{playerId}/card/{cardId}/play", api.ApiMiddleware(http.HandlerFunc(apiPlayer.PlayPlayerCard)))
 	http.Handle("POST /api/player/{playerId}/discard", api.ApiMiddleware(http.HandlerFunc(apiPlayer.DiscardPlayerHand)))
