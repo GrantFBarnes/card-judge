ea7b23e1681d74f5898b599b2ae3003ba049d09e
KVG <vgkaleb@gmail.com>
2024-09-21 22:35:28 -0500

    cleanup script case

--- database/dummy.sql
+++ database/dummy.sql
@@ -1,36 +1,38 @@
+USE CARD_JUDGE;
+
 INSERT INTO DECK (ID, NAME)
-VALUES (uuid(), 'Test 1'),
-       (uuid(), 'Test 2');
+VALUES (UUID(), 'Test 1'),
+       (UUID(), 'Test 2');
 
 
-select ID FROM DECK WHERE NAME = 'Test 1' into @DeckId1;
+SELECT ID FROM DECK WHERE NAME = 'Test 1' into @DeckId1;
 SELECT ID FROM CARD_TYPE WHERE NAME = 'Judge' into @JudgeCardId;
 SELECT ID FROM CARD_TYPE WHERE NAME = 'Player' into @PlayerCardId;
 
 
 INSERT INTO CARD (ID, DECK_ID, CARD_TYPE_ID, TEXT)
-VALUES (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 1'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 2'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 3'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 4'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 5'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 6'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 8'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 9'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 10'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 11'),
-       (uuid(), @DeckId1, @JudgeCardId,  'Judge Test 12'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 1'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 2'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 3'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 4'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 5'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 6'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 8'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 9'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 10'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 11'),
-       (uuid(), @DeckId1, @PlayerCardId,  'Player Test 12');
+VALUES (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 1'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 2'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 3'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 4'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 5'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 6'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 8'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 9'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 10'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 11'),
+       (UUID(), @DeckId1, @JudgeCardId,  'Judge Test 12'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 1'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 2'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 3'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 4'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 5'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 6'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 8'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 9'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 10'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 11'),
+       (UUID(), @DeckId1, @PlayerCardId,  'Player Test 12');
 
 
 
--- database/populate.sql
+++ database/populate.sql
@@ -4,8 +4,8 @@ INSERT INTO USER (NAME, PASSWORD_HASH, IS_ADMIN)
 VALUES ('Grant', '$2a$14$t7gWxR3Ak8uBkyPnw4TZz.WcN3nVlbDMEQgqHOuxEfWN3yCL3dgY.', 1);
 
 INSERT INTO CARD_TYPE (ID, NAME)
-VALUES (uuid(), 'Judge'),
-       (uuid(), 'Player');
+VALUES (UUID(), 'Judge'),
+       (UUID(), 'Player');
 
 
 
