2a246e102d9e94fd8ee7fbb44cc155d25a1c67c1
Grant Barnes <gfbarnes78@gmail.com>
2024-10-23 21:27:45 -0500

    fix duplicate decks in lobby create dialog

--- database/procedures/SP_GET_READABLE_DECKS.sql
+++ database/procedures/SP_GET_READABLE_DECKS.sql
@@ -15,7 +15,7 @@ BEGIN
         WHERE D.ID <> '00000000-0000-0000-0000-000000000000'
         ORDER BY D.NAME ASC;
     ELSE
-        SELECT
+        SELECT DISTINCT
             D.ID,
             D.NAME
         FROM DECK AS D
