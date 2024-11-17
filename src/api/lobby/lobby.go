2bb1d84c1d52b59fd15071d481c832a537ece5ad
Grant Barnes <gfbarnes78@gmail.com>
2024-11-17 08:39:06 -0600

    add bet on win special

--- src/api/lobby/lobby.go
+++ src/api/lobby/lobby.go
@@ -307,6 +307,32 @@ func GambleCredit(w http.ResponseWriter, r *http.Request) {
 	writeGameInterfaceHtml(w, player.Id)
 }
 
+func BetOnWin(w http.ResponseWriter, r *http.Request) {
+	lobbyIdString := r.PathValue("lobbyId")
+	lobbyId, err := uuid.Parse(lobbyIdString)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("Failed to get lobby id from path."))
+		return
+	}
+
+	player, err := getLobbyRequestPlayer(r, lobbyId)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	err = database.BetOnWin(player.Id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	writeGameInterfaceHtml(w, player.Id)
+}
+
 func AddExtraResponse(w http.ResponseWriter, r *http.Request) {
 	lobbyIdString := r.PathValue("lobbyId")
 	lobbyId, err := uuid.Parse(lobbyIdString)
--- src/main.go
+++ src/main.go
@@ -67,6 +67,7 @@ func main() {
 
 		// procedures
 		"../database/procedures/SP_ADD_EXTRA_RESPONSE.sql",
+		"../database/procedures/SP_BET_ON_WIN.sql",
 		"../database/procedures/SP_DISCARD_CARD.sql",
 		"../database/procedures/SP_DISCARD_HAND.sql",
 		"../database/procedures/SP_DRAW_HAND.sql",
@@ -184,6 +185,7 @@ func main() {
 	http.Handle("POST /api/lobby/{lobbyId}/card/{cardId}/play", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.PlayCard)))
 	http.Handle("POST /api/lobby/{lobbyId}/purchase-credits", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.PurchaseCredits)))
 	http.Handle("POST /api/lobby/{lobbyId}/gamble-credit", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.GambleCredit)))
+	http.Handle("POST /api/lobby/{lobbyId}/bet-on-win", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.BetOnWin)))
 	http.Handle("POST /api/lobby/{lobbyId}/add-extra-response", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.AddExtraResponse)))
 	http.Handle("POST /api/lobby/{lobbyId}/card/steal/play", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.PlayStealCard)))
 	http.Handle("POST /api/lobby/{lobbyId}/card/surprise/play", api.MiddlewareForAPIs(http.HandlerFunc(apiLobby.PlaySurpriseCard)))
