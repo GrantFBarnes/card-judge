b2376d80f0e8a176a8a3d412dd619c9b13a1a003
Grant Barnes <gfbarnes78@gmail.com>
2025-10-29 17:47:49 -0500

    pagination review - format documents

--- src/static/js/deck.js
+++ src/static/js/deck.js
@@ -1,17 +1,17 @@
 document.addEventListener("htmx:afterSwap", function (event) {
-    if (event.target.id === "deck-card-export-content") {
-        downloadCSV("card-judge-cards.csv", event.target.innerHTML);
-    }
+	if (event.target.id === "deck-card-export-content") {
+		downloadCSV("card-judge-cards.csv", event.target.innerHTML);
+	}
 });
 
 function downloadCSV(fileName, content) {
-    const element = document.createElement("a");
-    element.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(content));
-    element.setAttribute("download", fileName);
-    element.style.display = "none";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+	const element = document.createElement("a");
+	element.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(content));
+	element.setAttribute("download", fileName);
+	element.style.display = "none";
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
 }
 
 function goToPage(pageNum) {
