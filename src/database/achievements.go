d896f435c1919a1475dbb2d73d30e69c0c43037f
Grant Barnes <gfbarnes78@gmail.com>
2026-03-08 15:35:39 -0500

    redesign achievements

--- src/database/achievements.go
+++ src/database/achievements.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	"log"
@@ -10,10 +9,9 @@ import (
 )
 
 type Achievement struct {
-	Name         string
-	Progress     string
-	DateAchieved sql.NullTime
-	Rarity       string
+	Name     string
+	Achieved bool
+	Rarity   string
 }
 
 func GetAchievements(userId uuid.UUID) ([]Achievement, error) {
@@ -24,49 +22,28 @@ func GetAchievements(userId uuid.UUID) ([]Achievement, error) {
 		return result, err
 	}
 
-	userWinRoundCount, err := getUserWinRoundCount(userId)
-	if err != nil {
-		return result, err
-	}
-
 	sqlString := `
 		WITH USER_ACHIEVEMENTS AS (
 				SELECT
-					ACHIEVEMENTCODE,
-					CREATED_ON_DATE
+					ACHIEVEMENT_CODE
 				FROM USER_ACHIEVEMENT
 				WHERE USER_ID = ?
 			)
 		SELECT
 			A.NAME,
-			CASE
-				WHEN A.CODE = 'WIN-ROUND-1' THEN ? / 1
-				WHEN A.CODE = 'WIN-ROUND-10' THEN ? / 10
-				WHEN A.CODE = 'WIN-ROUND-100' THEN ? / 100
-				WHEN A.CODE = 'WIN-ROUND-1000' THEN ? / 1000
-				ELSE 0
-			END AS PROGRESS,
-			UA.CREATED_ON_DATE,
+			IF(UA.ACHIEVEMENT_CODE IS NOT NULL, 1, 0) AS ACHIEVED,
 			(
 				SELECT
 					COUNT(*) / ?
 				FROM USER_ACHIEVEMENT
-				WHERE ACHIEVEMENTCODE = A.CODE
+				WHERE ACHIEVEMENT_CODE = A.CODE
 			) AS RARITY
-		FROM ACHIEVEMENT AS A
-			LEFT JOIN USER_ACHIEVEMENTS AS UA ON UA.ACHIEVEMENTCODE = A.CODE
-		ORDER BY UA.CREATED_ON_DATE DESC,
+		FROM V_ACHIEVEMENT AS A
+			LEFT JOIN USER_ACHIEVEMENTS AS UA ON UA.ACHIEVEMENT_CODE = A.CODE
+		ORDER BY ACHIEVED DESC,
 			A.CODE
 	`
-	rows, err := query(
-		sqlString,
-		userId,
-		userWinRoundCount,
-		userWinRoundCount,
-		userWinRoundCount,
-		userWinRoundCount,
-		userCount,
-	)
+	rows, err := query(sqlString, userId, userCount)
 	if err != nil {
 		return result, err
 	}
@@ -74,19 +51,16 @@ func GetAchievements(userId uuid.UUID) ([]Achievement, error) {
 
 	for rows.Next() {
 		var achievement Achievement
-		var progress float32
 		var rarity float32
 		if err := rows.Scan(
 			&achievement.Name,
-			&progress,
-			&achievement.DateAchieved,
+			&achievement.Achieved,
 			&rarity,
 		); err != nil {
 			log.Println(err)
 			return result, errors.New("failed to scan row in query results")
 		}
 
-		achievement.Progress = getPercentageString(progress)
 		achievement.Rarity = getPercentageString(rarity)
 
 		result = append(result, achievement)
@@ -104,32 +78,6 @@ func getUserCount() (int, error) {
 		FROM USER
 	`
 	rows, err := query(sqlString)
-	if err != nil {
-		return result, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		if err := rows.Scan(&result); err != nil {
-			log.Println(err)
-			return result, errors.New("failed to scan row in query results")
-		}
-	}
-
-	return result, nil
-}
-
-func getUserWinRoundCount(userId uuid.UUID) (int, error) {
-	var result int
-
-	sqlString := `
-		SELECT
-			COUNT(DISTINCT LRC.ROUND_ID)
-		FROM LOG_RESPONSE_CARD AS LRC
-			INNER JOIN LOG_WIN AS LW ON LW.RESPONSE_ID = LRC.RESPONSE_ID
-		WHERE LRC.PLAYER_USER_ID = ?
-	`
-	rows, err := query(sqlString, userId)
 	if err != nil {
 		return result, err
 	}
