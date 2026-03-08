c0f4fa360f8eebc0ad861db94e702686e7542390
Grant Barnes <gfbarnes78@gmail.com>
2026-03-08 16:36:19 -0500

    add more achievements

--- src/static/sql/tables/USER_ACHIEVEMENT.sql
+++ src/static/sql/tables/USER_ACHIEVEMENT.sql
@@ -1,16 +1,28 @@
 CREATE TABLE IF NOT EXISTS USER_ACHIEVEMENT(
     USER_ID UUID NOT NULL,
     ACHIEVEMENT_CODE ENUM(
-        'KICK-1',
-        'KICK-10',
-        'KICK-20',
         'WIN-GAME-1',
         'WIN-GAME-10',
         'WIN-GAME-100',
         'WIN-ROUND-1',
         'WIN-ROUND-10',
         'WIN-ROUND-100',
-        'WIN-ROUND-1000'
+        'WIN-ROUND-1000',
+        'GAMBLE-1',
+        'GAMBLE-10',
+        'GAMBLE-20',
+        'GAMBLE-WIN-2',
+        'GAMBLE-WIN-20',
+        'GAMBLE-WIN-40',
+        'BET-1',
+        'BET-10',
+        'BET-20',
+        'BET-WIN-2',
+        'BET-WIN-20',
+        'BET-WIN-40',
+        'KICK-1',
+        'KICK-10',
+        'KICK-20'
     ) NOT NULL,
     PRIMARY KEY(USER_ID, ACHIEVEMENT_CODE),
     FOREIGN KEY(USER_ID) REFERENCES USER (ID) ON DELETE CASCADE
--- src/static/sql/views/V_ACHIEVEMENT.sql
+++ src/static/sql/views/V_ACHIEVEMENT.sql
@@ -34,6 +34,54 @@ FROM (
             'Win 1000 rounds' AS NAME
         UNION
         SELECT
+            'GAMBLE-1' AS CODE,
+            'Gamble 1 credit' AS NAME
+        UNION
+        SELECT
+            'GAMBLE-10' AS CODE,
+            'Gamble 10 credits' AS NAME
+        UNION
+        SELECT
+            'GAMBLE-20' AS CODE,
+            'Gamble 20 credits' AS NAME
+        UNION
+        SELECT
+            'GAMBLE-WIN-2' AS CODE,
+            'Win 2 credits gambling' AS NAME
+        UNION
+        SELECT
+            'GAMBLE-WIN-20' AS CODE,
+            'Win 20 credits gambling' AS NAME
+        UNION
+        SELECT
+            'GAMBLE-WIN-40' AS CODE,
+            'Win 40 credits gambling' AS NAME
+        UNION
+        SELECT
+            'BET-1' AS CODE,
+            'Bet 1 credit' AS NAME
+        UNION
+        SELECT
+            'BET-10' AS CODE,
+            'Bet 10 credits' AS NAME
+        UNION
+        SELECT
+            'BET-20' AS CODE,
+            'Bet 20 credits' AS NAME
+        UNION
+        SELECT
+            'BET-WIN-2' AS CODE,
+            'Win 2 credits betting' AS NAME
+        UNION
+        SELECT
+            'BET-WIN-20' AS CODE,
+            'Win 20 credits betting' AS NAME
+        UNION
+        SELECT
+            'BET-WIN-40' AS CODE,
+            'Win 40 credits betting' AS NAME
+        UNION
+        SELECT
             'KICK-1' AS CODE,
             'Get kicked' AS NAME
         UNION
