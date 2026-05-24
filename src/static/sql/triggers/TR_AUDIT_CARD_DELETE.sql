d6c74b445d4b25ea1b09ea2558c0351880697220
Grant Barnes <gfbarnes78@gmail.com>
2026-05-24 10:14:26 -0500

    do not put wild card deletes into audit card table

--- src/static/sql/triggers/TR_AUDIT_CARD_DELETE.sql
+++ src/static/sql/triggers/TR_AUDIT_CARD_DELETE.sql
@@ -3,22 +3,32 @@ OR REPLACE TRIGGER TR_AUDIT_CARD_DELETE
 BEFORE DELETE ON CARD
 FOR EACH ROW
 BEGIN
-    INSERT INTO AUDIT_CARD(
-        AUDIT_TYPE,
-        CARD_ID,
-        DECK_ID,
-        CATEGORY,
-        TEXT,
-        YOUTUBE,
-        IMAGE
-    )
-    VALUES (
-        'DELETE',
-        OLD.ID,
-        OLD.DECK_ID,
-        OLD.CATEGORY,
-        OLD.TEXT,
-        OLD.YOUTUBE,
-        OLD.IMAGE
-    );
+    DECLARE VAR_IS_WILD_CARD BOOLEAN DEFAULT (
+            SELECT
+                IS_LOBBY_WILD_DECK
+            FROM DECK
+            WHERE ID = OLD.DECK_ID
+        );
+
+    IF NOT VAR_IS_WILD_CARD THEN
+        INSERT INTO AUDIT_CARD(
+            AUDIT_TYPE,
+            CARD_ID,
+            DECK_ID,
+            CATEGORY,
+            TEXT,
+            YOUTUBE,
+            IMAGE
+        )
+        VALUES (
+            'DELETE',
+            OLD.ID,
+            OLD.DECK_ID,
+            OLD.CATEGORY,
+            OLD.TEXT,
+            OLD.YOUTUBE,
+            OLD.IMAGE
+        );
+    END
+    IF;
 END;
